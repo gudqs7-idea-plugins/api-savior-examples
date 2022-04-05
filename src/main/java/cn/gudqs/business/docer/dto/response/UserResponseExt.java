937c95d876c51abb9d521504d49145b672f70dd5
Guddqs <guddqs@icloud.com>
2022-04-05 19:46:45 +0800

    add

--- src/main/java/cn/gudqs/business/docer/dto/response/UserResponseExt.java
+++ src/main/java/cn/gudqs/business/docer/dto/response/UserResponseExt.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 用户信息2
@@ -32,5 +33,15 @@ public class UserResponseExt {
      * 收藏
      */
     private List<String> loveTagList;
+
+    /**
+     * 测试 map
+     */
+    private Map<String, Object> otherMap;
+
+    /**
+     * 测试 list
+     */
+    private List<Object> objectList;
 
 }
