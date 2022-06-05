91a51474209337230498b9b7202b8a76b0de4011
gudqs7 <guddqs@icloud.com>
2022-06-05 22:21:47 +0800

    feat(test): 新增测试错误接口

---
+++ src/main/java/cn/gudqs/test/docer/controller/TestErrorController.java
@@ -0,0 +1,25 @@
+package cn.gudqs.test.docer.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 测试错误接口
+ * #hidden
+ */
+@RestController
+@RequestMapping("/test/error")
+public class TestErrorController {
+
+    /**
+     * 测试示例值类型转换失败
+     *
+     * @param deleted 删除 example=false
+     * @param age     年龄 example=一百
+     */
+    @RequestMapping("/testErrorExample")
+    public void testErrorExample(Boolean deleted, Integer age) {
+
+    }
+
+}
